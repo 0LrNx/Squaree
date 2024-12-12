9d00fbf07e8396eb21b210c9bdf648ccf5b90da2
0LrNx <FISSSSSS@proton.me>
2024-12-12 21:32:09 +0100

    feat: Partners

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/src/components/Navbar/Navbar";
 import TextHero from "@/app/src/components/Hero/textHero"
 import ImageHero from "@/app/src/components/Hero/imgHero"
+import Parteners from "@/app/src/components/Parteners/parteners"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Navbar />
     <TextHero />
     <ImageHero />
+    <Parteners />
   </main>
   )
 }
---
+++ app/src/components/Parteners/parteners.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import Bubble from "@/public/featured/Bubble.svg";
+import Fluid from "@/public/featured/Fluid.svg";
+import Hikkeno from "@/public/featured/Hikkeno.svg";
+
+function Parteners() {
+    return (
+        <div className="flex flex-col justify-center items-center text-dark text-xl font-dmsans font-medium pt-32">
+            <h2 className="mb-8">We have the fast paced growing companies with us</h2>
+            <div className="flex gap-40">
+                <Image
+                    src={Fluid}
+                    alt="Bubble"
+                />
+
+                <Image
+                    src={Hikkeno}
+                    alt="Fluid"
+                />
+
+                <Image
+                    src={Bubble}
+                    alt="Hikkeno"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Parteners;
